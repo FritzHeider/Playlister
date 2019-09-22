75be34bc40d557e4fafa9b9c5adb135ac82d6e2a
FritzHeider <fritz.heider@students.makeschool.com>
2019-09-22 13:42:46 -0700

    is mongo working?
    is mongo working>

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client= MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
 
 # OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    { 'title': 'Doggo Videos', 'description': 'Doggoss acting weird'},
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing! '},
-    { 'title': '70\'s Music', 'description': 'All wrapped up in Blue! '},
-    { 'title': '60\'s Music', 'description': 'Lay lady lay '}
-]
+# playlists = [
+#     { 'title': 'Doggo Videos', 'description': 'Doggoss acting weird'},
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing! '},
+#     { 'title': '70\'s Music', 'description': 'All wrapped up in Blue! '},
+#
+#
+#     { 'title': '60\'s Music', 'description': 'Lay lady lay '}
+# ]
 
 @app.route('/')
 def playlists_index():
     """Show all playlits."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
