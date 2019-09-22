838fef9db54b4b8379ac3bcf1695c5be176c2d8e
FritzHeider <fritz.heider@students.makeschool.com>
2019-09-22 12:27:44 -0700

    Users can see all playlists

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Return Homepage."""
-    return render_template('home.html', msg='Yay working with flask')
 
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird'},
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing! '}
+]
+
+@app.route('/')
+def playlists_index():
+    """Show all playlits."""
+    return render_template('playlists_index.html', playlists=playlists)
 
 if __name__ == '__main__':
     app.run(debug=True)
